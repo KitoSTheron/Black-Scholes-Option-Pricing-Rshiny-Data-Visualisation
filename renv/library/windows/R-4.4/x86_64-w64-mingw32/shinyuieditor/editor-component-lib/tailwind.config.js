a28fd2495ae2a05b539b147ca3d4fcea706899ad
KitoSTheron <145708071+KitoSTheron@users.noreply.github.com>
2025-03-19 21:25:17 +0000

    Revert "cleanup"
    This reverts commit bf315e5b690d1bdc919cd7fb50422576b01b87e9.

--- renv/library/windows/R-4.4/x86_64-w64-mingw32/shinyuieditor/editor-component-lib/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-import twConfig from "../shared-configs/tailwind.config";
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...twConfig,
-  content: ["../editor/index.html", "../editor/src/**/*.{ts,tsx}"],
-};
